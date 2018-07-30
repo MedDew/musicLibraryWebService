ee992a63b07387f8ed8d99af888cc5e779c3422e
Med <mehdi.gacem@versusmind.eu>
2018-07-30 11:37:54 +0200

    Create Music service interface to interact with the Music object

---
+++ src/main/java/com/musiclibrary/musiclibrarywebservice/service/MusicManager.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.musiclibrary.musiclibrarywebservice.service;
+
+import com.musiclibrary.musiclibraryapi.dto.MusicDTO;
+import java.util.List;
+
+/**
+ *
+ * @author Mehdi
+ */
+public interface MusicManager 
+{
+    List<MusicDTO> getMusics();
+    
+    MusicDTO insertMusic(MusicDTO musicDTO);
+    
+    MusicDTO updateMusic(MusicDTO musicDTO, long id);
+    
+    MusicDTO deleteMusicById(long id);
+    
+    MusicDTO findById(long id);
+}
