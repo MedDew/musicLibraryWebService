63c85358a1098f38d388254a7701b75e5bbb638f
Med <mehdi.gacem@versusmind.eu>
2018-07-30 18:01:24 +0200

    Add stereotype annotation to be considered as a service and take the advantage of IoC

--- src/main/java/com/musiclibrary/musiclibrarywebservice/service/MusicManagerService.java
+++ src/main/java/com/musiclibrary/musiclibrarywebservice/service/MusicManagerService.java
@@ -13,11 +13,15 @@ import com.musiclibrary.musiclibrarywebservice.repository.MusicRepository;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author Mehdi
  */
+@Service
+@Transactional
 public class MusicManagerService implements MusicManager 
 {
 
