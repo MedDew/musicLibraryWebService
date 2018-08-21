a83cefa042c07db76e026fea4b95de9ae948425c
Med <mehdi.gacem@versusmind.eu>
2018-08-21 16:17:32 +0200

    Create enum to have a selection of predefined unique constraint

---
+++ src/main/java/com/musiclibrary/musiclibrarywebservice/ExceptionMessageType.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.musiclibrary.musiclibrarywebservice;
+
+/**
+ *
+ * @author Mehdi
+ */
+public enum ExceptionMessageType 
+{
+    GENRE("UK_GenreName"),//GenreException
+    CATEGORY("UK_CategoryName");//CategoryException
+    
+    private String exceptionMessageTypeName;
+    
+    ExceptionMessageType(String name)
+    {
+        this.exceptionMessageTypeName = name;
+    }
+    
+    public static ExceptionMessageType findExceptionMessageType(String type)
+    {
+        ExceptionMessageType foundExceptionMessageType = null;
+        for(ExceptionMessageType ex : ExceptionMessageType.values())
+        {
+            System.out.println("ExceptionMessageType : "+ex);
+            System.out.println("ExceptionMessageType 2  : "+ex.GENRE);
+            System.out.println("ExceptionMessageType 2  : "+ex.GENRE.getClass().getName());
+            System.out.println("ExceptionMessageType 2  : "+ex.GENRE.toString().getClass().getName());
+            System.out.println("ExceptionMessageType 2  : "+GENRE.toString().getClass().getTypeName());
+            System.out.println("ExceptionMessageType 2  : "+GENRE.toString().getClass().getSimpleName());
+            System.out.println(type.equals(ex.GENRE.toString()));
+            if(type.equals(ex.GENRE.toString()))
+            {
+                System.out.println(" FOUND Genre Type : "+ex.GENRE.name());
+                System.out.println(" FOUND Genre Type : "+GENRE.getClass().getCanonicalName());
+                
+                foundExceptionMessageType = ex.GENRE;
+                break;
+            }
+            else if(type.equals(ex.CATEGORY.toString()))
+            {
+                System.out.println(" FOUND Category Type : "+ex.CATEGORY.name());
+                foundExceptionMessageType = ex.CATEGORY;
+            }
+        }
+        
+        return foundExceptionMessageType;
+    }
+
+    @Override
+    public String toString() {
+        return this.exceptionMessageTypeName;
+    }
+}
